431872654d52d2bf50c401472a983f59c6fbd461
gudipudiswaroop <gudipudiswaroop@gmail.com>
2018-09-21 00:14:07 +0530

    Create DBUtils.java

---
+++ DBUtils.java
@@ -0,0 +1,25 @@
+package wiseguy;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBUtils {
+	
+	private static Connection conn = null;
+	
+	private static DBConnection connection = new DBConnection();
+
+	public static Connection getDBConnection() {
+		
+		
+		try {
+			if(conn == null || conn.isClosed()) {
+				conn = connection.getConnection();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return conn;
+	}
+}
